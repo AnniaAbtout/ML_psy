bade24c532bc999ddb5f38b10b2c17053d2d1904
Morgan Saunier <morgan.saunier.gg@gmail.com>
2024-08-20 16:31:13 +0200

    base params

---
+++ params.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+
+DATA_SIZE = os.environ.get("DATA_SIZE")
+CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE"))
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_REGION = os.environ.get("GCP_REGION")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+INSTANCE = os.environ.get("INSTANCE")
