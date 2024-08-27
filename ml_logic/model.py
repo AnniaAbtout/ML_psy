4a2a56dbdf601e4c116133ce3c553eea825f0ca3
Annia Abtout <a.abtout@live.fr>
2024-08-27 17:24:15 +0200

    updated model

--- ml_logic/model.py
+++ ml_logic/model.py
@@ -48,17 +48,20 @@ def model_anxiety_disorder(data : pd.DataFrame):
     X_preprocessed = preprocess_features(X)
     y_preprocessed = preprocess_target(pd.DataFrame(y), 'Anxiety disorder')
 
-    #define the model : random forest classifier
-    rf_anxiety_disorder = RandomForestClassifier('colsample_bytree': 0.9,
-                                                'learning_rate': 0.01,
-                                                'max_depth': 7,
-                                                'n_estimators': 100,
-                                                'subsample': 0.9)
+    #define the model : xgboost
+    xgb_anxiety_disorder = XGBClassifier(objective='binary:logistic',
+                                            eval_metric='logloss',
+                                            use_label_encoder=False,
+                                            colsample_bytree = 0.9,
+                                            learning_rate = 0.01,
+                                            max_depth = 7,
+                                            n_estimators = 100,
+                                            subsample = 0.9)
 
     #train the model
-    rf_anxiety_disorder.fit(X_preprocessed, y_preprocessed)
+    xgb_anxiety_disorder.fit(X_preprocessed, y_preprocessed)
 
-    return rf_anxiety_disorder
+    return xgb_anxiety_disorder
 
 def model_mood_disorder(data : pd.DataFrame):
 
@@ -76,12 +79,15 @@ def model_mood_disorder(data : pd.DataFrame):
     X_preprocessed = preprocess_features(X)
     y_preprocessed = preprocess_target(pd.DataFrame(y), 'Mood disorder')
 
-    #define the model : xgBoost
-    xgb_mood_disorder = XGBClassifier('colsample_bytree': 0.8,
-                                        'learning_rate': 0.1,
-                                        'max_depth': 7,
-                                        'n_estimators': 200,
-                                        'subsample': 0.9)
+    #define the model : xgboost
+    xgb_mood_disorder = XGBClassifier(objective='binary:logistic',
+                                            eval_metric='logloss',
+                                            use_label_encoder=False,
+                                            colsample_bytree = 0.8,
+                                            learning_rate = 0.1,
+                                            max_depth = 7,
+                                            n_estimators = 200,
+                                            subsample = 0.9)
 
     #train the model
     xgb_mood_disorder.fit(X_preprocessed, y_preprocessed)
@@ -108,11 +114,11 @@ def model_schizophrenia(data : pd.DataFrame):
     xgb_schizophrenia = XGBClassifier(objective='binary:logistic',
                                             eval_metric='logloss',
                                             use_label_encoder=False,
-                                            'colsample_bytree': 0.8,
-                                            'learning_rate': 0.2,
-                                            'max_depth': 7,
-                                            'n_estimators': 200,
-                                            'subsample': 0.9)
+                                            colsample_bytree = 0.8,
+                                            learning_rate = 0.2,
+                                            max_depth = 7,
+                                            n_estimators = 200,
+                                            subsample = 0.9)
 
     #train the model
     xgb_schizophrenia.fit(X_preprocessed, y_preprocessed)
@@ -136,13 +142,13 @@ def model_trauma_and_stress_related_disorder(data : pd.DataFrame):
     X_preprocessed = preprocess_features(X)
     y_preprocessed = preprocess_target(pd.DataFrame(y), 'Trauma and stress related disorder')
 
-    #define the model : xgBoost
-    xgb_trauma = XGBClassifier('criterion': 'entropy',
-                                'max_depth': 5,
-                                'max_features': 'sqrt',
-                                'n_estimators': 200)
+    #define the model : rf
+    rf_trauma_and_stress = RandomForestClassifier(criterion =  'entropy',
+                                                    max_depth = 5,
+                                                    max_features = 'sqrt',
+                                                    n_estimators = 200)
 
     #train the model
-    xgb_trauma.fit(X_preprocessed, y_preprocessed)
+    rf_trauma_and_stress.fit(X_preprocessed, y_preprocessed)
 
-    return xgb_trauma
+    return rf_trauma_and_stress
